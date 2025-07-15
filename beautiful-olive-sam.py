d90905111701ef4b73dfaf31794b286fbc461a71
memebot50000 <memebot50000@gmail.com>
2025-07-14 20:06:45 -0400

    Update beautiful-olive-sam.py

--- beautiful-olive-sam.py
+++ beautiful-olive-sam.py
@@ -1,19 +1,22 @@
 import time
 import random
-import pyttsx3
-
+from playsound import playsound
 #say positive things every ten minutes
 #say positive things
 def sayPositiveThings():
     #print("You are going to get your kip soon!")
-    robotVoice = pyttsx3.init()
-    robotVoice.say(random.choice(positiveThings))
-    engine.runAndWait()
+    #print(random.choice(positiveThings))
+    playsound(random.choice(positiveThings))
+
 
 
 #main program
-positiveThings = ["You are going to get your kip soon!", "blah blah blah blah", "oogah shakah nah", "koalas are cute", "rorororrororo"]
-numberOfNewThings= int(input("How many new positive things would you like me to say?"))
+#positiveThings = ["You are going to get your kip soon!", "blah blah blah blah", "oogah shakah nah", "koalas are cute", "rorororrororo"]
+#numberOfNewThings= int(input("How many new positive things would you like me to say?"))
+numberOfThings = 3
+positiveThings = []
+for r in range(0, numberOfThings): # :)
+    positiveThings.append("thing" + str(r) + ".mp3")
 
 # i = 0
 # while (i <= numberOfNewThings):
@@ -22,19 +25,13 @@ numberOfNewThings= int(input("How many new positive things would you like me to 
 #     i += 1 
 # print(positiveThings)
 
-for i in range (0, numberOfNewThings):
-    newThing = str(input("Input new positive thing here:"))
-    positiveThings.append(newThing)
-print(positiveThings)
+# for i in range (0, numberOfNewThings):
+#     newThing = str(input("Input new positive thing here:"))
+#     positiveThings.append(newThing)
+# print(positiveThings)
 
 while True:
     #time.sleep(6000)
     time.sleep(6)
     sayPositiveThings()
-
-
-
-
-#control LEDs
-#control speakers
 
