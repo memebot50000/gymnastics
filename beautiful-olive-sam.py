e0e947b93f98daea3ed337f0753321ea3ea19155
memebot50000 <memebot50000@gmail.com>
2025-07-16 19:53:15 -0400

    Update beautiful-olive-sam.py

--- beautiful-olive-sam.py
+++ beautiful-olive-sam.py
@@ -39,7 +39,7 @@ for r in range(0, numberOfThings): # :)
 # print(positiveThings)
 
 while True:
-    #time.sleep(6000)
-    time.sleep(6)
+    time.sleep(6000)
+    #time.sleep(6)
     sayPositiveThings()
 
